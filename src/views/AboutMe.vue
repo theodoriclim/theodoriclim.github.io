ca770c8fac2d6b153a00f97e4dca7485eb4c6188
Theodoric <theodorickeithlim@gmail.com>
2022-03-13 20:36:34 +0800

    Update AboutMe

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,17 +1,62 @@
 <template>
-  <div class="fullscreen">
-    <h1>ABOUT ME</h1>
-  </div>
+  <v-layout justify-center class="fullscreen">
+    <v-container>
+      <title-text
+        class="mb-12"
+        justify="center"
+        main="About Me."
+        secondary="Profile"
+        divider-color="var(--app-blue)"
+        divider-width="5.75rem"
+      />
+      <v-row>
+        <v-col col="1" class="profile-text">
+          Hi! I am Theodoric, friends call me Theo. I started off in information
+          security but found my passion in web development. Apart from learning
+          about software frameworks and the likes, I enjoy keeping myself fit.
+          So far, I have gotten into weightlifting, roller blading, ice skating
+          and as of late, rock climbing.
+        </v-col>
+        <v-col col="1" class="profile-text">
+          Being in NUS, it has also provided me with the opportunity to share my
+          love for programming by teaching classes to juniors in my course. I
+          look forward to be able to contribute to a company, and improve myself
+          as a developer. Continue scrolling to see some of my skillsets and
+          works!
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-layout>
 </template>
 
-<script>
-export default {
-  name: "AboutMe",
-};
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import TitleText from "../components/TitleText.vue";
+
+@Component({
+  components: {
+    TitleText,
+  },
+})
+export default class AboutMe extends Vue {}
 </script>
 
 <style scoped>
 .fullscreen {
   background-color: var(--app-white);
+}
+
+@media (min-width: 1264px) {
+  .fullscreen > .container {
+    padding-top: 30vh;
+    margin: 0 5rem;
+  }
+
+  .profile-text {
+    font-size: var(--font-size-nine);
+    line-height: 1.25rem;
+    text-align: justify;
+  }
 }
 </style>
