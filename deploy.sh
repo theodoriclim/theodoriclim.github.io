6f1f759c0839a495d97c15e47d5e23e9b214a69b
Theodoric <theodorickeithlim@gmail.com>
2022-04-10 08:00:33 +0800

    Update project settings

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,15 @@
 # abort on errors
 set -e
 # build
+export NODE_OPTIONS=--openssl-legacy-provider
 yarn run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
-git add -A
+git remote add origin https://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@github.com/cowturtle/cowturtle.github.io.git
+git add .
 git commit -m 'deploy'
-git push -f git@github.com:cowturtle/cowturtle.git master:gh-pages
-cd -
+git push -f origin master:gh-pages
+cd ..
